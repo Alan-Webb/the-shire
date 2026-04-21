866ffefc8a50252de64213e6570cc529176023df
Alan Webb <webbalan91939@gmail.com>
2026-04-22 00:33:32 +0100

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import {useState} from "react";
-import {FaHome, FaUtensils, FaListAlt, FaQuoteLeft} from "react-icons/fa";
+import {
+	FaHome,
+	FaUtensils,
+	FaListAlt,
+	FaQuoteLeft,
+	FaSearch,
+	FaShoppingCart,
+	FaUserAlt,
+} from "react-icons/fa";
 import {BiSolidBowlHot} from "react-icons/bi";
 
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const menuItems = [
-		{icon: <FaHome />, text: "Home"},
-		{icon: <FaUtensils />, text: "Services"},
-		{icon: <FaListAlt />, text: "Menu"},
-		{icon: <FaQuoteLeft />, text: "Reviews"},
+		{href: "/", icon: <FaHome />, text: "Home"},
+		{href: "#services", icon: <FaUtensils />, text: "Services"},
+		{href: "#menu", icon: <FaListAlt />, text: "Menu"},
+		{href: "#reviews", icon: <FaQuoteLeft />, text: "Reviews"},
 	];
 
 	return (
@@ -19,7 +27,42 @@ const Navbar = () => {
 				<div className="p-2 bg-white/60 rounded-full backdrop-blur-sm">
 					<BiSolidBowlHot className="text-2xl text-black animate-pulse" />
 				</div>
-				<h1 className="text-xl lg:text-3xl font-semibold">The Shire</h1>
+				<h1 className="font-extrabold">The Shire</h1>
+			</div>
+			{/* DESKTOP NAVIGATION */}
+			<div className="hidden md:flex space-x-4 lg:space-x-8">
+				{menuItems.map((item, index) => (
+					<a
+						key={index}
+						href={item.href}
+						className="flex items-center space-x-2 text-black font-semibold hover:text-orange-800 transition-all duration-300 cursor-pointer">
+						<span>{item.icon}</span>
+						<span>{item.text}</span>
+					</a>
+				))}
+			</div>
+			{/* RIGHT SECTION */}
+			<div className="flex items-center space-x-3">
+				{/* Search Bar */}
+				<div className="relative hidden sm:block">
+					<input
+						type="text"
+						name="search"
+						placeholder="Search..."
+						className="w-28 md:w-32 lg:w-48 border border-gray-300 rounded-full py-1 pl-3 pr-10 text-sm focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent transition duration-300"
+					/>
+					<FaSearch className="absolute right-3 top-1.75 transform hover:scale-120 transition-transform duration-300" />
+				</div>
+				{/* Cart & Login */}
+				<div className="flex items-center space-x-3">
+					<button className="p-2 text-xl bg-white/20 rounded-full hover:bg-white/30  hover:scale-110 transition-all focus:outline-none cursor-pointer border border-gray-600">
+						<FaShoppingCart />
+					</button>
+					<button className="hidden md:flex items-center space-x-2 bg-linear-to-br from-yellow-300 to-orange-400 px-2 sm:px-0.5 lg:px-4 py-2 rounded-full hover:shadow-lg hover:scale-110 transition-all focus:outline-none whitespace-nowrap cursor-pointer">
+						<FaUserAlt className="w-4 h-4" />
+						<span className="font-semibold text-white text-sm ">Login</span>
+					</button>
+				</div>
 			</div>
 		</nav>
 	);
