47f73a398b32f8e6b9701fdb626d64d417040a4a
Alan Webb <webbalan91939@gmail.com>
2026-05-26 08:02:38 +0100

    Create Services.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Services from "./components/Services";
 
 const App = () => {
 	return (
@@ -8,6 +9,7 @@ const App = () => {
 			<Navbar />
 			<Hero />
 			<About />
+			<Services />
 		</>
 	);
 };
